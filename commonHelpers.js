468dc55162d4adb131d8c96c3573135f584bd45f
VeronikaSupriaha <146442956+VeronikaSupriaha@users.noreply.github.com>
2024-03-24 13:09:36 +0000

    Deploying to gh-pages from @ VeronikaSupriaha/goit-js-hw-11@80959e4dae9050d520d93dcc0899876c389ec639 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,12 @@
+import{i as d,S as u}from"./assets/vendor-5b791d57.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))s(r);new MutationObserver(r=>{for(const e of r)if(e.type==="childList")for(const n of e.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function i(r){const e={};return r.integrity&&(e.integrity=r.integrity),r.referrerPolicy&&(e.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?e.credentials="include":r.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function s(r){if(r.ep)return;r.ep=!0;const e=i(r);fetch(r.href,e)}})();function p(o){return fetch(o).then(t=>t.json())}function f(o,t,i){if(o.length<=0)throw new Error(d.show({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",backgroundColor:"#ef4040",position:"topRight"}));const s=o.map(e=>`
+          <li class="cards">
+          <a href="${e.largeImageURL}"> <img src="${e.webformatURL}" alt="${e.tags}" width="360" height="152"/> </a>
+          <div class="card-description">
+            <p class="description"><b>Likes</b> ${e.likes}</p>
+            <p class="description"><b>Views</b> ${e.views}</p>
+            <p class="description"><b>Comments</b> ${e.comments}</p>
+            <p class="description"><b>Downloads</b> ${e.downloads}</p>
+            </div>
+          </li>
+      `).join("");t.insertAdjacentHTML("beforeend",s);const r=new u(".gallery a",{captionsData:"alt",captionDelay:250});i.classList.add("is-hidden"),r.refresh()}const a=document.querySelector("#data-input"),m=document.querySelector("[data-search]"),c=document.querySelector(".gallery"),l=document.querySelector(".loader");m.addEventListener("click",o=>{if(o.preventDefault(),l.classList.remove("is-hidden"),c.innerHTML="",a.value===""){d.show({title:"Error",message:"Please, fill the field to start search images. ",backgroundColor:"#ef4040",position:"topRight"});return}const i=`https://pixabay.com/api/?${new URLSearchParams({key:"43027284-ae8275f6d108abc1bc2578323",q:a.value,image_type:"photo",orientation:"horizontal",safesearch:!0})}`;p(i).then(s=>f(s.hits,c,l)).catch(s=>console.log(s))});
+//# sourceMappingURL=commonHelpers.js.map
